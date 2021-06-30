7a287984f908ed50d762bc589588858b54bcbfca
Abid Hassan <abidhassandiba@gmail.com>
2021-06-30 20:36:35 +0600

    Section1: Setup & Migration

---
+++ AkijFood/Migrations/20210630143430_AddTblColdDrinksToDB.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AkijFood.Migrations
+{
+    public partial class AddTblColdDrinksToDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tblColdDrinks",
+                columns: table => new
+                {
+                    ColdDrinksId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ColdDrinksName = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    NumQuantity = table.Column<decimal>(type: "decimal(4)", nullable: false),
+                    NumUnitPrice = table.Column<decimal>(type: "decimal(3)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblColdDrinks", x => x.ColdDrinksId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tblColdDrinks");
+        }
+    }
+}
---
+++ AkijFood/Models/tblColdDrink.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AkijFood.Models
+{
+    public class tblColdDrink
+    {
+        [Key]
+        public int ColdDrinksId { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(50)")]
+        public string ColdDrinksName { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(4)")]
+        public int NumQuantity { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(3)")]
+        public int NumUnitPrice { get; set; }
+      
+    }
+}
