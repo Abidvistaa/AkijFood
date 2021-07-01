b32c52c784a9502658b0b9aae02f24ab1a05f585
Abid Hassan <abidhassandiba@gmail.com>
2021-07-01 11:22:09 +0600

    Section5: API5 & API6

---
+++ AkijFood/Controllers/API5AndAPI6Controller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AkijFood.Data;
+using AkijFood.Models;
+
+namespace AkijFood.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class API5AndAPI6Controller : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public API5AndAPI6Controller(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        //API 05: Delete All products if it’s quantity is less than 500
+        [HttpPost]
+        public async Task<IActionResult> DeleteLessThan()
+        {
+            var itemtoremove = _context.tblColdDrinks.Where(x => x.NumQuantity < 500).First();
+
+            _context.tblColdDrinks.Remove(itemtoremove);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpGet]
+        //API 06: Find total price of all products
+        public ActionResult TotalPrice()
+        {
+            var itemtoremove = _context.tblColdDrinks.Sum(x => x.NumUnitPrice);
+            return Ok(itemtoremove);
+        }
+    }
+}
